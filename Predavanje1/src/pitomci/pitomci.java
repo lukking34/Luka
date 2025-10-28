c90882dbb9ebc1ccc8c5990861c17007de24b073
lukking34 <116023590+lukking34@users.noreply.github.com>
2025-10-28 23:06:16 +0100

    Add files via upload

---
+++ Predavanje1/src/pitomci/pitomci.java
@@ -0,0 +1,17 @@
+// kazuvame kade e smesten .java klasata
+package pitomci; 
+//zadavame ime na public klasa sto ja narekuvame pitomci
+public class pitomci 
+//pocetok na klasata
+{ 
+
+	// go kreirame glavniot medot main
+	public static void main(String[] args) 
+	//pocetok na main 
+	{ 
+		//ispecatuvame na konzola "Ние сме питомци од 29та класа"
+		System.out.println("Ние сме питомци од 29та класа");
+// kraj na main
+	}
+//kraj na klasata
+}
---
+++ Predavanje1/src/zdravo/zdravo.java
@@ -0,0 +1,12 @@
+package zdravo;
+
+public class zdravo 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Здраво питомци");
+		
+	}
+
+}
