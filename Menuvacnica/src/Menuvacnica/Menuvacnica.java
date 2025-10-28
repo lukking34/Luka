ef44ac4f9a45f98193be20a2a651245d8922a829
lukking34 <116023590+lukking34@users.noreply.github.com>
2025-10-28 22:37:50 +0100

    Add files via upload

---
+++ Menuvacnica/src/Menuvacnica/Menuvacnica.java
@@ -0,0 +1,12 @@
+package Menuvacnica;
+public class Menuvacnica 
+{ 	
+	public static void main(String[] args) 
+	{	int evra = 110;
+		double denari = evra * 61.5;
+		System.out.print("Denari:");
+		System.out.print(denari);
+
+	}
+
+}
