12600897c13b1c541d1b166a978194fbc61a4e71
lukking34 <116023590+lukking34@users.noreply.github.com>
2025-10-28 22:57:31 +0100

    Add files via upload

---
+++ Ovosje/src/ovosje/Ovosje.java
@@ -0,0 +1,19 @@
+package ovosje;
+
+public class Ovosje {
+
+	public static void main(String[] args) {
+		double jabolkikg = 2.0;
+		double kompirikg = 5.0;
+		double praskikg = 2.5;
+		
+		int cenajab = 32;
+		int cenakom = 20;
+		int cenapra = 50;
+		
+		double suma =  jabolkikg*cenajab + kompirikg *cenakom + praskikg*cenapra;
+		
+	 System.out.print("Cenata e: " + suma + "den");
+	}
+
+}
