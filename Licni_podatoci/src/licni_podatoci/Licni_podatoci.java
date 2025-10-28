5570c2933c47ce77d68afdfb990caee140eacb9b
lukking34 <116023590+lukking34@users.noreply.github.com>
2025-10-28 22:51:06 +0100

    Add files via upload

---
+++ Licni_podatoci/src/licni_podatoci/Licni_podatoci.java
@@ -0,0 +1,17 @@
+package licni_podatoci;
+
+public class Licni_podatoci {
+
+    public static void main(String[] args) {
+        String ime = "Luka";
+        String prezime = "Pajovic";
+        double tocni_godini = 20.5;
+        String grad = "Skopje";
+        String krvna_grupa = "A+";
+        
+        
+        System.out.println("Zdravo, ja sum " + ime + " " + prezime + ", jas sum tocno "
+                           + tocni_godini + " godini, doagjam od " + grad + 
+                           ". Mojata krvna grupa e " + krvna_grupa + ".");
+    }
+}
